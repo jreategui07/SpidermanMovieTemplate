fe43f6d08af439d22e41c95cf41c3e610d230257
Jonathan Reátegui <j.reategui2012@gmail.com>
2024-05-24 16:38:04 -0400

    updating films list data and images

--- js/films.js
+++ js/films.js
@@ -1,40 +1,62 @@
 const FILMS = [
     {
-        "name": "Hollywood Film Festival",
-        "description": "A prestigious festival showcasing the best in international cinema.",
-        "date": "October 1-7, 2024",
-        "location": "Los Angeles, USA",
-        "image_url": "img/spiderman-poster.jpg"
+        "name": "Spider-Man",
+        "description": "Peter Parker becomes Spider-Man to fight evil.",
+        "date": "Friday, May 3, 2002, 8:00 PM",
+        "location": "Mann Village Theatre, Los Angeles",
+        "image_url": "img/spider-man-1-poster.jpg"
     },
     {
-        "name": "Hollywood Film Festival",
-        "description": "A prestigious festival showcasing the best in international cinema.",
-        "date": "October 1-7, 2024",
-        "location": "Los Angeles, USA",
-        "image_url": "img/spiderman-poster.jpg"
+        "name": "Spider-Man 2",
+        "description": "Spider-Man faces new challenges and foes.",
+        "date": "Wednesday, June 30, 2004, 7:30 PM",
+        "location": "Mann Village Theatre, Los Angeles",
+        "image_url": "img/spider-man-2-poster.jpg"
     },
     {
-        "name": "Hollywood Film Festival",
-        "description": "A prestigious festival showcasing the best in international cinema.",
-        "date": "October 1-7, 2024",
-        "location": "Los Angeles, USA",
-        "image_url": "img/spiderman-poster.jpg"
+        "name": "Spider-Man 3",
+        "description": "Spider-Man battles against Venom and Sandman.",
+        "date": "Monday, April 30, 2007, 7:00 PM",
+        "location": "Astor Plaza Theatre, New York City",
+        "image_url": "img/spider-man-3-poster.jpg"
     },
     {
-        "name": "Hollywood Film Festival",
-        "description": "A prestigious festival showcasing the best in international cinema.",
-        "date": "October 1-7, 2024",
-        "location": "Los Angeles, USA",
-        "image_url": "img/spiderman-poster.jpg"
+        "name": "The Amazing Spider-Man",
+        "description": "A new story of Peter Parker as Spider-Man.",
+        "date": "Tuesday, June 13, 2012, 7:00 PM",
+        "location": "Sony Pictures Studios, Culver City",
+        "image_url": "img/the-amazing-spider-man-poster.jpg"
     },
     {
-        "name": "Hollywood Film Festival",
-        "description": "A prestigious festival showcasing the best in international cinema.",
-        "date": "October 1-7, 2024",
-        "location": "Los Angeles, USA",
-        "image_url": "img/spiderman-poster.jpg"
+        "name": "The Amazing Spider-Man 2",
+        "description": "Spider-Man faces Electro and his past.",
+        "date": "Thursday, April 24, 2014, 7:00 PM",
+        "location": "Ziegfeld Theatre, New York City",
+        "image_url": "img/the-amazing-spider-man-2-poster.jpg"
+    },
+    {
+        "name": "Spider-Man: Homecoming",
+        "description": "Peter Parker balances high school life and being Spider-Man.",
+        "date": "Wednesday, June 28, 2017, 7:00 PM",
+        "location": "TCL Chinese Theatre, Los Angeles",
+        "image_url": "img/spider-man-homecoming-poster.jpg"
+    },
+    {
+        "name": "Spider-Man: Far From Home",
+        "description": "Spider-Man goes on a European adventure.",
+        "date": "Wednesday, June 26, 2019, 7:00 PM",
+        "location": "TCL Chinese Theatre, Los Angeles",
+        "image_url": "img/spider-man-far-from-home-poster.jpg"
+    },
+    {
+        "name": "Spider-Man: No Way Home",
+        "description": "Spider-Man faces multiversal threats.",
+        "date": "Monday, December 13, 2021, 7:00 PM",
+        "location": "Fox Village Theatre, Los Angeles",
+        "image_url": "img/spider-man-no-way-home-poster.jpg"
     }
 ]
+
 
 const loadFilms = () => {
     for (let i = 0; i < FILMS.length; i++) {
