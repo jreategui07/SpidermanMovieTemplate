0d151e6b0c318a2a8f7d1b91b9cbea2d9b97a837
Jonathan Reátegui <j.reategui2012@gmail.com>
2024-05-24 01:23:48 -0400

    validations of data and optimizations

--- js/purchase-ticket.js
+++ js/purchase-ticket.js
@@ -11,10 +11,6 @@ const TICKETS_PRICE = [
     }
 ]
 
-let ticketType = ""
-let numberTicktes = ""
-let creditCard = ""
-
 const cleanForm = () => {
     document.querySelector("#select-ticket-type").value = ""
     document.querySelector("#txt-number-ticktes").value = ""
@@ -30,42 +26,56 @@ const buyTickts = () => {
 const btnOrderSummary = () => {
     document.querySelector("#order-summary-list").innerHTML = ""
 
-    // check values
-    ticketType = document.querySelector("#select-ticket-type").value
-    numberTicktes = document.querySelector("#txt-number-ticktes").value
-    creditCard = document.querySelector("#txt-credit-card").value
+    const FORM_DATA = {
+        ticketType: document.querySelector("#select-ticket-type").value,
+        numberTicktes: document.querySelector("#txt-number-ticktes").value,
+        creditCard: document.querySelector("#txt-credit-card").value
+    }
 
-    if (ticketType.trim() === "" || numberTicktes.trim() === "" || creditCard.trim() === "") {
-        alert("ERROR: Should provide ticket type, number of tickets hours credit card information")
+    if (
+        FORM_DATA.ticketType.trim() === "" ||
+        FORM_DATA.numberTicktes.trim() === "" ||
+        FORM_DATA.creditCard.trim() === ""
+    ) {
+        alert("WARNING: Should provide ticket type, number of tickets hours credit card information")
         return
     }
 
-    if (creditCard.length != 6) {
-        alert("ERROR: CreditCard Should contain 6 digits");
+    if (FORM_DATA.creditCard.length != 6) {
+        alert("WARNING: CreditCard Should contain 6 digits");
         return
     }
 
     document.querySelector("#orderSummary").style.display = "flex";
     
     let ticketPrice = 35.99
-    if (ticketType === "ODP") {
+    if (FORM_DATA.ticketType === "ODP") {
         ticketPrice = 95.00
-    } else if (ticketType === "ODP") {
+    } else if (FORM_DATA.ticketType === "ODP") {
         ticketPrice = 274.00
     }
 
-    let subtotal = ticketPrice * parseFloat(numberTicktes)
+    let subtotal = ticketPrice * parseFloat(FORM_DATA.numberTicktes)
     let tax = subtotal * 0.13
     let finalPrice = subtotal + tax
 
+    const ORDER_SUMMARY = {
+        ticketType: FORM_DATA.ticketType,
+        numberTicktes: FORM_DATA.numberTicktes,
+        ticketPrice: ticketPrice.toFixed(2),
+        subtotal: subtotal.toFixed(2),
+        tax: tax.toFixed(2),
+        finalPrice: finalPrice.toFixed(2)
+    }
+
     // calculate receipt
     document.querySelector("#order-summary-list").innerHTML += `
-            <a>Ticket type: <strong>${ ticketType }</strong></a>
-            <a>Number of tickets: <strong>${ numberTicktes }</strong></a>
-            <a>Price per ticket: <strong>$${ ticketPrice.toFixed(2) }</strong></a>
-            <a>Subtotal: <strong>$${ subtotal.toFixed(2) }</strong></a>
-            <a>Tax (13%): <strong>$${ tax.toFixed(2) }</strong></a>
-            <a>Final Price: <strong>$${ finalPrice.toFixed(2) }</strong></a>
+            <a>Ticket type: <strong>${ ORDER_SUMMARY.ticketType }</strong></a>
+            <a>Number of tickets: <strong>${ ORDER_SUMMARY.numberTicktes }</strong></a>
+            <a>Price per ticket: <strong>$${ ORDER_SUMMARY.ticketPrice }</strong></a>
+            <a>Subtotal: <strong>$${ ORDER_SUMMARY.subtotal }</strong></a>
+            <a>Tax (13%): <strong>$${ ORDER_SUMMARY.tax }</strong></a>
+            <a>Final Price: <strong>$${ ORDER_SUMMARY.finalPrice }</strong></a>
             <button class="btn-primary" id="btn-pay">Confirm and pay</button>
     `;
     
